5d9338794b161763e83c6df12afec033864fd19f
amit kumar gupta <amitgupta.gwl@gmail.com>
2024-09-03 14:27:15 +0530

    test for FOLLOW

---
+++ tests/follow_test.js
@@ -0,0 +1,111 @@
+const FlowParser = require("../src/FlowParser"); 
+const {customDeepEqual,toSafeString} = require("./util"); 
+
+describe("Flow Parser", function() {
+  it("should parse flow with FOllOW", function() {
+    const flowText = `
+        FLOW: Addition
+        Add both values
+
+        FLOW: Sample flow 1
+        version: 1.0
+        threshold: 5000
+        LOOP condition 1
+          DO A
+          IF condition 2
+            FOLLOW Addition
+          DO B
+        DO C`;
+
+    const expected = {
+        "Addition": {
+          "name": "Addition",
+          "headers": {
+            "version": 1,
+            "threshold": 7000
+          },
+          "steps": [
+            {
+              "msg": "Add both values",
+              "nextStep": [],
+              "index": 0,
+              "type": ""
+            }
+          ],
+          "index": {
+            "0": "Point to step 0: Add both values"
+          },
+          "exitSteps": [
+            "Point to step 0: Add both values"
+          ]
+        },
+        "Sample flow 1": {
+          "name": "Sample flow 1",
+          "headers": {
+            "version": 1,
+            "threshold": 5000
+          },
+          "steps": [
+            {
+              "msg": "condition 1",
+              "nextStep": [
+                {
+                  "msg": "DO A",
+                  "nextStep": [
+                    {
+                      "msg": "condition 2",
+                      "nextStep": [
+                        {
+                          "msg": "Addition",
+                          "nextStep": [
+                            {
+                              "msg": "DO B",
+                              "nextStep": [
+                                "Point to step 0: LOOP condition 1"
+                              ],
+                              "index": 4,
+                              "type": ""
+                            }
+                          ],
+                          "index": 3,
+                          "type": "FOLLOW"
+                        },
+                        "Point to step 4: DO B"
+                      ],
+                      "index": 2,
+                      "type": "IF"
+                    }
+                  ],
+                  "index": 1,
+                  "type": ""
+                },
+                {
+                  "msg": "DO C",
+                  "nextStep": [],
+                  "index": 5,
+                  "type": ""
+                }
+              ],
+              "index": 0,
+              "type": "LOOP"
+            }
+          ],
+          "index": {
+            "0": "Point to step 0: LOOP condition 1",
+            "1": "Point to step 1: DO A",
+            "2": "Point to step 2: IF condition 2",
+            "3": "Point to step 3: FOLLOW Addition",
+            "4": "Point to step 4: DO B",
+            "5": "Point to step 5: DO C"
+          },
+          "exitSteps": [
+            "Point to step 5: DO C"
+          ]
+        }
+      };
+    const parser = new FlowParser();
+    const flows = parser.parse(flowText);
+    // console.log(toSafeString(flows));
+    expect(customDeepEqual(flows,expected)).toBeTrue();
+  });
+});
